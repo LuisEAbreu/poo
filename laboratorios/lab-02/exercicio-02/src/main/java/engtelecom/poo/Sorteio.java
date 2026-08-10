10a6e4aef0a1d93c838263613205eef071fabad9
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-10 17:10:57 -0300

    Lab 02 - Exercício 02 concluído

--- laboratorios/lab-02/exercicio-02/src/main/java/engtelecom/poo/Sorteio.java
+++ laboratorios/lab-02/exercicio-02/src/main/java/engtelecom/poo/Sorteio.java
@@ -1,7 +1,26 @@
 package engtelecom.poo;
 
+import java.util.Random;
+
 public class Sorteio {
     static void main() {
+        Random aleatorio = new Random();
+        int sorteado = aleatorio.nextInt(1,100);
 
+        int palpite;
+        int tentativas = 0;
+
+        do {
+            palpite = Integer.parseInt(IO.readln("Entre com o palpite: "));
+
+            if (palpite != sorteado) {
+                String mensagem = String.format("Você errou. O número sorteado é %s que o palpite", (sorteado > palpite) ? "maior" : "menor");
+                IO.println(mensagem);
+                tentativas++;
+            }
+        } while (palpite != sorteado);
+
+        String saida = String.format("Parabéns, você acertou! \nUsou %d tentativas",tentativas);
+        IO.println(saida);
     }
 }
