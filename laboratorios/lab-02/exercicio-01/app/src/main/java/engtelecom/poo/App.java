871e7d8e949e8145e43335cb6f9cabf1daa435b8
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-10 16:26:14 -0300

    Lab 02 - Exercício 01 concluído

--- laboratorios/lab-02/exercicio-01/app/src/main/java/engtelecom/poo/App.java
+++ laboratorios/lab-02/exercicio-01/app/src/main/java/engtelecom/poo/App.java
@@ -3,23 +3,20 @@ package engtelecom.poo;
 public class App {
     public static void main(String[] args) {
         int dia = Integer.parseInt(IO.readln("Entre com o dia: "));
-        String mesEscrito = IO.readln("Entre com o mês: ");
+        String mes = IO.readln("Entre com o mês: ");
 
-        /*
-        Outono: Começa em 20 de março e termina em 21 de junho.
-        (dia >= 20 && mes >= 3) && (dia < 21 && mes <= 6)
-
-        Inverno: Começa em 21 de junho e termina em 22 de setembro.
-        (dia >= 21 && mes >= 6) && (dia < 22 && mes <= 9)
-
-        Primavera: Começa em 22 de setembro e termina em 21 de dezembro.
-        (dia >= 22 && mes >= 9) && (dia < 21 && mes <= 12)
-
-        Verão: Começa em 21 de dezembro e termina em 20 de março
-        (dia >= 20 && mes >= 3) && (dia < 20 && mes <= 3)
-
-        */
-
-        IO.println("");
+        String estacao = switch (mes.toLowerCase()){
+            case "janeiro","fevereiro" -> "verão";
+            case "março" -> (dia < 20) ? "verão" : "outono";
+            case "abril", "maio" -> "outono";
+            case "junho" -> (dia < 21) ? "outono" : "inverno";
+            case "julho", "agosto" -> "inverno";
+            case "setembro" -> (dia < 22) ? "inverno" : "primavera";
+            case "outubro", "novembro" -> "primavera";
+            case "dezembro" -> (dia < 21) ? "primavera" : "verão";
+            default -> "mês inválido";
+        };
+        String mensagem = String.format("No Brasil, o dia %d de %s é %s",dia, mes, estacao);
+        IO.println(mensagem);
     }
 }
