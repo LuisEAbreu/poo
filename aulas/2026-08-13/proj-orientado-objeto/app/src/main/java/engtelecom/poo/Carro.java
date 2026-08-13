5bac636be38f5749c9b5f326dcbd88c74d36d3f0
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-13 17:18:23 -0300

    Criando classe Carro

---
+++ aulas/2026-08-13/proj-orientado-objeto/app/src/main/java/engtelecom/poo/Carro.java
@@ -0,0 +1,15 @@
+package engtelecom.poo;
+
+public class Carro {
+    // atributos:
+    private String cor;
+
+    // métodos:
+    public void definirCor(String novaCor){
+        cor = novaCor;
+    }
+
+    public String obterCor(){
+        return cor;
+    }
+}
