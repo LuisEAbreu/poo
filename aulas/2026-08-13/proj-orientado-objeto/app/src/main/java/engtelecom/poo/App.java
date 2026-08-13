a6e0382c11843895481abaa66c099b6c50a81ec6
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-13 17:18:50 -0300

    Testando no main a classe carro e alguns métodos

--- aulas/2026-08-13/proj-orientado-objeto/app/src/main/java/engtelecom/poo/App.java
+++ aulas/2026-08-13/proj-orientado-objeto/app/src/main/java/engtelecom/poo/App.java
@@ -2,6 +2,12 @@ package engtelecom.poo;
 
 public class App {
     static void main() {
+        Carro fusca = new Carro();
+        Carro ferrari = new Carro();
 
+        fusca.definirCor("azul");
+
+        IO.println(fusca.obterCor());
+        IO.println(ferrari.obterCor());
     }
 }
