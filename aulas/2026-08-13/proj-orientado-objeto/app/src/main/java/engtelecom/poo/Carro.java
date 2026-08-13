c1de3302ecfc3dc9b950eed7ad05799c83d67c38
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-13 17:34:02 -0300

    Criando método para obter velocidade atual do Carro

--- aulas/2026-08-13/proj-orientado-objeto/app/src/main/java/engtelecom/poo/Carro.java
+++ aulas/2026-08-13/proj-orientado-objeto/app/src/main/java/engtelecom/poo/Carro.java
@@ -3,13 +3,18 @@ package engtelecom.poo;
 public class Carro {
     // atributos:
     private String cor;
+    private int velocidadeAtual;
 
     // métodos:
-    public void definirCor(String novaCor){
+    public void definirCor(String novaCor) {
         cor = novaCor;
     }
 
-    public String obterCor(){
+    public String obterCor() {
         return cor;
+    }
+
+    public int obterVelocidadeAtual() {
+        return velocidadeAtual;
     }
 }
