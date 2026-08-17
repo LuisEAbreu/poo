9fc118d0b1ba4af38bab814b38731c7bdad94aee
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-17 17:21:58 -0300

    Modelando a classe Batedeira

--- aulas/2026-08-17/exercicio-03/app/src/main/java/engtelecom/poo/Batedeira.java
+++ aulas/2026-08-17/exercicio-03/app/src/main/java/engtelecom/poo/Batedeira.java
@@ -1,4 +1,40 @@
 package engtelecom.poo;
 
 public class Batedeira {
+    private double preco;
+    private String cor;
+    private String marca;
+    private int potencia;
+
+    public void definirPreco(double valor){
+        preco = valor;
+    }
+
+    public void definirCor(String novaCor){
+        cor = novaCor;
+    }
+
+    public void definirMarca(String novaMarca){
+        marca = novaMarca;
+    }
+
+    public void definirPotencia(int valor){
+        potencia = valor;
+    }
+
+    public double preco(){
+        return preco;
+    }
+
+    public String cor(){
+        return cor;
+    }
+
+    public String marca(){
+        return marca;
+    }
+
+    public int potencia(){
+        return potencia;
+    }
 }
