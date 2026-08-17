70db0c3fb5cd5d4b9f56091bc62666b93a5c02ed
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-17 16:28:56 -0300

    Adicionando métodos para atribuir valor ao Contador

--- aulas/2026-08-17/exercicio-01/app/src/main/java/engtelecom/poo/Contador.java
+++ aulas/2026-08-17/exercicio-01/app/src/main/java/engtelecom/poo/Contador.java
@@ -2,4 +2,8 @@ package engtelecom.poo;
 
 public class Contador {
     private int valorAtual;
+
+    public void atribuirValor(int valor){
+        valorAtual = (valor >= 0) ? valor : valorAtual;
+    }
 }
