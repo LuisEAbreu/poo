1344b1e05ca2295dfa21e465f38dd09bb43427df
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-17 17:00:33 -0300

    Modelando classe Personagem

--- aulas/2026-08-17/exercicio-02/app/src/main/java/engtelecom/poo/Personagem.java
+++ aulas/2026-08-17/exercicio-02/app/src/main/java/engtelecom/poo/Personagem.java
@@ -1,4 +1,33 @@
 package engtelecom.poo;
 
 public class Personagem {
+    private String nome;
+    private int vida = 100;
+    private String classeCombate;
+
+    public void definirNome(String entrada){
+        nome = entrada;
+    }
+
+    public void dano(int quantidadeDano){
+        if(quantidadeDano > 0){
+            vida = Math.max(vida - quantidadeDano, 0);
+        }
+    }
+
+    public void definirClasse(String classe){
+        classeCombate = classe;
+    }
+
+    public String nome(){
+        return nome;
+    }
+
+    public int vitalidade(){
+        return vida;
+    }
+
+    public String consultarClasse(){
+        return classeCombate;
+    }
 }
