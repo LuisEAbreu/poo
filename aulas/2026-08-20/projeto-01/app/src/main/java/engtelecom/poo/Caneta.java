3d28e52a20480535030fe4d747d7ba243ca4cd9a
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-20 17:10:37 -0300

    Criando getters e setters da classe Caneta

--- aulas/2026-08-20/projeto-01/app/src/main/java/engtelecom/poo/Caneta.java
+++ aulas/2026-08-20/projeto-01/app/src/main/java/engtelecom/poo/Caneta.java
@@ -16,4 +16,20 @@ public class Caneta {
     public Caneta() {
         this("preta");
     }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getNivelTinta() {
+        return nivelTinta;
+    }
+
+    public void setNivelTinta(double nivelTinta) {
+        this.nivelTinta = Math.min(Math.max(0, nivelTinta),100);
+    }
 }
