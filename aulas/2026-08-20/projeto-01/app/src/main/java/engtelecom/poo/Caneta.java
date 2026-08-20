ac2a7a8b525421e93934bb8ceb81a14688e31896
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-20 17:10:03 -0300

    Criando constructors da classe Caneta

--- aulas/2026-08-20/projeto-01/app/src/main/java/engtelecom/poo/Caneta.java
+++ aulas/2026-08-20/projeto-01/app/src/main/java/engtelecom/poo/Caneta.java
@@ -3,4 +3,17 @@ package engtelecom.poo;
 public class Caneta {
     private String cor;
     private double nivelTinta;
+
+    public Caneta(String cor, double nivelTinta) {
+        this.cor = cor;
+        this.nivelTinta = nivelTinta;
+    }
+
+    public Caneta(String cor) {
+        this(cor, 100);
+    }
+
+    public Caneta() {
+        this("preta");
+    }
 }
