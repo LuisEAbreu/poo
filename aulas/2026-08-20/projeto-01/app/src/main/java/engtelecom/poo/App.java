bed7165609ceb8341baf1962a163bdf1a9023cb2
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-20 17:27:33 -0300

    Testando classe Caneta no main

--- aulas/2026-08-20/projeto-01/app/src/main/java/engtelecom/poo/App.java
+++ aulas/2026-08-20/projeto-01/app/src/main/java/engtelecom/poo/App.java
@@ -2,6 +2,20 @@ package engtelecom.poo;
 
 public class App {
     static void main(String[] args) {
+        Caneta bic = new Caneta("Azul");
 
+        bic.setNivelTinta(12);
+        IO.println(bic.getNivelTinta());
+        bic.setNivelTinta(3);
+        IO.println(bic.getNivelTinta());
+        bic.setNivelTinta(120);
+        IO.println(bic.getNivelTinta());
+        bic.setNivelTinta(-30);
+        IO.println(bic.getNivelTinta());
+        bic.setNivelTinta(0.01);
+        IO.println(bic.getNivelTinta());
+
+        IO.println(bic.desenhar(1, 3, 3 , 5));
+        IO.println(bic.getNivelTinta());
     }
 }
