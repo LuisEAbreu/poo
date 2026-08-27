ddfb839dcd35a525cedbade8704fbfb00dc10e09
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-08-27 16:36:22 -0300

    Lab 03 - Exercício 01 concluído

--- laboratorios/lab-03/exercicio-01/app/src/main/java/engtelecom/poo/App.java
+++ laboratorios/lab-03/exercicio-01/app/src/main/java/engtelecom/poo/App.java
@@ -2,6 +2,12 @@ package engtelecom.poo;
 
 public class App {
     static void main(String[] args) {
+        Ponto a = new Ponto(3, 5);
+        Ponto b = new Ponto(1, 4);
 
+        IO.println("Ponto a: "+ a);
+        IO.println("Ponto b: "+ b);
+
+        IO.println("Distância do ponto a até o ponto b é: " + a.distancia(b));
     }
 }
