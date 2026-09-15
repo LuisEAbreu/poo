17ac86d24bc17d36f70ee86fd82c5fd80675f81b
luise <luis.e2003@aluno.ifsc.edu.br>
2026-09-15 17:48:32 -0300

    Criando projeto usando ArrayList e seus utilitários

---
+++ aulas/2026-09-14/app/src/main/java/engtelecom/poo/Doc.java
@@ -0,0 +1,36 @@
+package engtelecom.poo;
+
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+
+        ArrayList<String> lista = new ArrayList<>();
+
+        lista.add("POO");
+        lista.add("IFSC");
+
+        String s = lista.get(1);
+
+        // for i
+        for (int i = 0; i < lista.size(); i++) {
+            IO.println(lista.get(i));
+        }
+
+        // for each
+        for (String elemento : lista){
+            IO.println(elemento);
+        }
+
+        // usando lambda
+        lista.forEach(elemento-> IO.println(elemento));
+
+        // lambda com mais de uma instrução
+        lista.forEach(e-> {
+            IO.println(e);
+        });
+
+        // method reference
+        lista.forEach(IO::println);
+    }
+}
