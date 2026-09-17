ea19dbaaf4b7eba5c372fc969c6bdfd7a27dfd2e
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-09-17 16:11:44 -0300

    Exemplificando uso de tabela hash no projeto

--- aulas/2026-09-17/exemplo1/app/src/main/java/engtelecom/poo/App.java
+++ aulas/2026-09-17/exemplo1/app/src/main/java/engtelecom/poo/App.java
@@ -1,7 +1,21 @@
 package engtelecom.poo;
 
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) {
-        
+//               (chave, valor)
+        HashMap <String, Pessoa> mapa = new HashMap<>();
+
+        mapa.put("123", new Pessoa("Flavinho", "flavinho@do.pneu"));
+        mapa.put("456", new Pessoa("Shaolin", "shaolin@matador.deporco"));
+        mapa.put("789", new Pessoa("Fulano", "fulano@example.org"));
+
+        mapa.forEach((c,v) -> IO.println("Chave: " + c + ", valor: " + v));
+
+        var removida = mapa.remove("123");
+        IO.println("Pessoa removida: " + removida);
+
+        mapa.values().removeIf(e -> e.getNome().equals("Shaolin"));
     }
 }
